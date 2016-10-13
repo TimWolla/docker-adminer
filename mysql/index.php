ac494c7b859f944e6fc174ba64ef09477f256ddf
Tim Düsterhus <tim@bastelstu.be>
2016-10-13 19:38:12 +0200

    Allow to choose the server name

--- mysql/index.php
+++ mysql/index.php
@@ -1,3 +1,26 @@
 <?php
-define('SERVER', 'db');
-require('adminer.php');
+namespace docker {
+	function adminer_object() {
+		class Adminer extends \Adminer {
+			function loginForm() {
+				ob_start();
+				$return = parent::loginForm();
+				$form = ob_get_clean();
+
+				echo str_replace('name="auth[server]" value="" title="hostname[:port]"', 'name="auth[server]" value="db" title="hostname[:port]"', $form);
+
+				return $return;
+			}
+		}
+
+		return new Adminer();
+	}
+}
+
+namespace {
+	function adminer_object() {
+		return \docker\adminer_object();
+	}
+
+	require('adminer.php');
+}
