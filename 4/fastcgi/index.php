62c4f1701962632773ae5f28361a7c8b118f5a4f
strowi <strowi@hasnoname.de>
2025-04-24 14:54:15 +0200

    style(indentation): tabs to spaces

--- 4/fastcgi/index.php
+++ 4/fastcgi/index.php
@@ -1,64 +1,64 @@
 <?php
 namespace docker {
-	function adminer_object() {
-		require_once('plugins/plugin.php');
+  function adminer_object() {
+    require_once('plugins/plugin.php');
 
-		class Adminer extends \AdminerPlugin {
-			function _callParent($function, $args) {
-				if ($function === 'loginForm') {
-					ob_start();
-					$return = \Adminer::loginForm();
-					$form = ob_get_clean();
+    class Adminer extends \AdminerPlugin {
+      function _callParent($function, $args) {
+        if ($function === 'loginForm') {
+          ob_start();
+          $return = \Adminer::loginForm();
+          $form = ob_get_clean();
 
-					// Set default values via env vars.
-					$defaultDbDriver = getenv('ADMINER_DEFAULT_DRIVER') ?: 'server';
-					$defaultDbHost = getenv('ADMINER_DEFAULT_SERVER') ?: '';
-					$defaultDb = getenv('ADMINER_DEFAULT_DBNAME') ?: '';
+          // Set default values via env vars.
+          $defaultDbDriver = getenv('ADMINER_DEFAULT_DRIVER') ?: 'server';
+          $defaultDbHost = getenv('ADMINER_DEFAULT_SERVER') ?: '';
+          $defaultDb = getenv('ADMINER_DEFAULT_DBNAME') ?: '';
 
-					$defaultDbDriver = $defaultDbDriver == 'mysql' ? 'server' : $defaultDbDriver;
+          $defaultDbDriver = $defaultDbDriver == 'mysql' ? 'server' : $defaultDbDriver;
 
-					echo str_replace(
-							[
-									'name="auth[server]" value="" title="hostname[:port]"',
-									'value="' . $defaultDbDriver . '"',
-									'selected="">MySQL',
-									'name="auth[db]" value=""'
-							],
-							[
-									'name="auth[server]" value="' . $defaultDbHost . '" title="hostname[:port]"',
-									'value="' . $defaultDbDriver . '" selected="selected"',
-									'>MySQL',
-									'name="auth[db]" value="' . $defaultDb . '"'
-							],
-							$form
-					);
+          echo str_replace(
+              [
+                  'name="auth[server]" value="" title="hostname[:port]"',
+                  'value="' . $defaultDbDriver . '"',
+                  'selected="">MySQL',
+                  'name="auth[db]" value=""'
+              ],
+              [
+                  'name="auth[server]" value="' . $defaultDbHost . '" title="hostname[:port]"',
+                  'value="' . $defaultDbDriver . '" selected="selected"',
+                  '>MySQL',
+                  'name="auth[db]" value="' . $defaultDb . '"'
+              ],
+              $form
+          );
 
-					return $return;
-				}
+          return $return;
+        }
 
-				return parent::_callParent($function, $args);
-			}
-		}
+        return parent::_callParent($function, $args);
+      }
+    }
 
-		$plugins = [];
-		foreach (glob('plugins-enabled/*.php') as $plugin) {
-			$plugins[] = require($plugin);
-		}
+    $plugins = [];
+    foreach (glob('plugins-enabled/*.php') as $plugin) {
+      $plugins[] = require($plugin);
+    }
 
-		return new Adminer($plugins);
-	}
+    return new Adminer($plugins);
+  }
 }
 
 namespace {
-	if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
-		header('Content-Type: text/css');
-		readfile('adminer.css');
-		exit;
-	}
+  if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
+    header('Content-Type: text/css');
+    readfile('adminer.css');
+    exit;
+  }
 
-	function adminer_object() {
-		return \docker\adminer_object();
-	}
+  function adminer_object() {
+    return \docker\adminer_object();
+  }
 
-	require('adminer.php');
+  require('adminer.php');
 }
--- 5/fastcgi/index.php
+++ 5/fastcgi/index.php
@@ -1,10 +1,10 @@
 <?php
 namespace docker {
-	function adminer_object() {
-		/**
-		 * Prefills the “Server”, "Driver", "DBName" fields with 
+  function adminer_object() {
+    /**
+     * Prefills the “Server”, "Driver", "DBName" fields with 
      * the ADMINER_DEFAULT_(SERVER,DRIVER,DBNAME) environment variables.
-		 */
+     */
     final class DefaultServerPlugin extends \Adminer\Plugin {
       public function __construct(
         private \Adminer\Adminer $adminer
@@ -39,36 +39,36 @@ namespace docker {
       }
     }
 
-		$plugins = [];
-		foreach (glob('plugins-enabled/*.php') as $plugin) {
-			$plugins[] = require($plugin);
-		}
+    $plugins = [];
+    foreach (glob('plugins-enabled/*.php') as $plugin) {
+      $plugins[] = require($plugin);
+    }
 
-		$adminer = new \Adminer\Plugins($plugins);
+    $adminer = new \Adminer\Plugins($plugins);
 
-		(function () {
-			$last = &$this->hooks['loginFormField'][\array_key_last($this->hooks['loginFormField'])];
-			if ($last instanceof \Adminer\Adminer) {
-				$defaultServerPlugin = new DefaultServerPlugin($last);
-				$this->plugins[] = $defaultServerPlugin;
-				$last = $defaultServerPlugin;
-			}
-		})->call($adminer);
+    (function () {
+      $last = &$this->hooks['loginFormField'][\array_key_last($this->hooks['loginFormField'])];
+      if ($last instanceof \Adminer\Adminer) {
+        $defaultServerPlugin = new DefaultServerPlugin($last);
+        $this->plugins[] = $defaultServerPlugin;
+        $last = $defaultServerPlugin;
+      }
+    })->call($adminer);
 
-		return $adminer;
-	}
+    return $adminer;
+  }
 }
 
 namespace {
-	if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
-		header('Content-Type: text/css');
-		readfile('adminer.css');
-		exit;
-	}
+  if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
+    header('Content-Type: text/css');
+    readfile('adminer.css');
+    exit;
+  }
 
-	function adminer_object() {
-		return \docker\adminer_object();
-	}
+  function adminer_object() {
+    return \docker\adminer_object();
+  }
 
-	require('adminer.php');
+  require('adminer.php');
 }
